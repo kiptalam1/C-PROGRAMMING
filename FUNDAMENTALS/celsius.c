b8d579038f8dddde51d83b6bbab0352b5e67825e
kiptalam1 <adamskiptalam0@gmail.com>
2025-11-26 19:33:16 +0300

    Defining names for constants using '#define'

---
+++ FUNDAMENTALS/celsius.c
@@ -0,0 +1,22 @@
+/*
+Converts a Fahrenheit temperature to Celsius
+*/
+
+#include <stdio.h>
+
+#define FREEZING_POINT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main (void)
+{
+  float fahrenheit, celsius;
+
+  printf("Enter Fahrenheit temperature: ");
+  scanf("%f", &fahrenheit);
+
+  celsius = (fahrenheit - FREEZING_POINT) * SCALE_FACTOR;
+
+  printf("Celsius equivalent: %.1f\n", celsius);
+
+  return 0;
+}
