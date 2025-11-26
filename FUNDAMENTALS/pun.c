f9108b0206b9288b62e668ed4d695c0440e3924f
kiptalam1 <adamskiptalam0@gmail.com>
2025-11-26 15:51:46 +0300

    comments in C

--- FUNDAMENTALS/pun.c
+++ FUNDAMENTALS/pun.c
@@ -1,11 +1,12 @@
+// Name: pun.c
+// Purpose: Print a bad pun
+// Author: Adams Kiptalam
+
 #include <stdio.h>
 
 int main(void) 
 {
   printf("To C, or not to C: that is the question.\n");
-
-  // printf("To C, or not to C: ");
-// printf("that is the question.\n");
   printf("Brevity is the soul of wit.\n  --Shakespeare\n");
   return 0;
 }
