9abea4a892c0525fb7a1db761daccc2f7c66ae66
kiptalam1 <adamskiptalam0@gmail.com>
2025-12-19 18:24:29 +0300

    pointers for array processing

---
+++ ADVANCED/POINTERS/sum_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define N 4
+
+int main (void) {
+  int a[N] = {11,34,82};
+ int  sum, *p;
+  sum = 0;
+
+  for (p = &a[0]; p < &a[N]; p++) {
+    sum += *p;
+  }
+
+  printf("Sum = %d\n", sum);
+}
